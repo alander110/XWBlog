011dba5909a1c87ea466c1bb05cc1c627438e6a7
alander110 <wy1366368016@gmail.com>
2025-04-02 23:33:06 +0800

    博客后端项目初始化

---
+++ src/main/java/com/xiaowang/xwblog/XwBlogApplication.java
@@ -0,0 +1,13 @@
+package com.xiaowang.xwblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XwBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XwBlogApplication.class, args);
+    }
+
+}
